18ba643859e4c8b63c977af29605dab0bb5814cd
Trish Griego <pmgriego@gmail.com>
2016-09-15 14:11:49 -0700

    all your kitties are back and showing up

--- recursion.rb
+++ recursion.rb
@@ -1,6 +1,37 @@
 
 def fact(n)
+  if n == 1 || n == 0
+    return 1
+  else
+    return n * fact(n-1)
+  end
 end
+
+# mystery(123)
+# mystery(9005)
+def mystery(n)
+  if n < 10
+    return n
+  else
+    return (n%10) + mystery(n/10)
+  end
+end
+# myster2(13)
+#mystery2(-6)
+
+def myster2(s)
+  if n == 0 
+    return 100
+  elsif n == -1
+    return 200
+  end
+  if n % 2 == 0
+    return myster2(n/2)
+  else
+    return myster2(n-1)
+  end
+end
+
 
 def reverse(s)
 end
